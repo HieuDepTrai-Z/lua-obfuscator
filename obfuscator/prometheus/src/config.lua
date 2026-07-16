590a75cf495a52e494c8221f4b8cb1905c613373
Dev-HieuRoblox <hieudepzaicubu@gmail.com>
2026-07-16 11:36:08 +0700

    Create config.lua

---
+++ obfuscator/prometheus/src/config.lua
@@ -0,0 +1,9 @@
+-- This Script is Part of the Prometheus Obfuscator by levno-710
+--
+-- config.lua
+--
+-- Provides shared configuration values (e.g. branding name used in logger output)
+
+return {
+    NameUpper = "PROMETHEUS",
+}
